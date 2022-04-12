06587cb88069433b1aed529241bae746e81243b0
Nirbose <nirbose2@gmail.com>
2022-04-12 18:47:59 +0200

    up Map.php

--- src/Map.php
+++ src/Map.php
@@ -36,4 +36,90 @@ class Map extends Collection
         return $filtered;
     }
 
+    /**
+     * Reduce collection by callback
+     * 
+     * @param callable $callback
+     * @param mixed $initial
+     * @return mixed
+     */
+    public static function reduce(callable $callback, $initial = null)
+    {
+        $reduced = $initial;
+
+        foreach (self::$items as $key => $item) {
+            $reduced = $callback($reduced, $key, $item);
+        }
+
+        return $reduced;
+    }
+
+    /**
+     * Find item in collection by callback
+     * 
+     * @param callable $callback
+     * @return mixed
+     */
+    public static function find(callable $callback)
+    {
+        foreach (self::$items as $key => $item) {
+            if ($callback($key, $item)) {
+                return $item;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Find key in collection by callback
+     * 
+     * @param callable $callback
+     * @return mixed
+     */
+    public static function findKey(callable $callback)
+    {
+        foreach (self::$items as $key => $item) {
+            if ($callback($key, $item)) {
+                return $key;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Find value in collection by callback
+     * 
+     * @param callable $callback
+     * @return mixed
+     */
+    public static function findValue(callable $callback)
+    {
+        foreach (self::$items as $key => $item) {
+            if ($callback($key, $item)) {
+                return $item;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Check if collection contains item by callback
+     * 
+     * @param callable $callback
+     * @return bool
+     */
+    public static function contains(callable $callback): bool
+    {
+        foreach (self::$items as $key => $item) {
+            if ($callback($key, $item)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
